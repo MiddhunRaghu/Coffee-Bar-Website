269ba47b94753dfff6725a1740e714e6061dff17
MiddhunRaghu <middhunraghu@gmail.com>
2025-06-02 12:15:31 +0530

    package update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/coffee-bar-website/', // Set the base path for the app
+  base: '/coffee-bar-website/' // Set the base path for the app
 })
